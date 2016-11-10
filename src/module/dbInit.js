97368da87992fe0e44cc22334e60fc22ff02c7b3
flowke <flowke@163.com>
2016-11-10 21:45:38 +0800

    restructure for all app

--- src/module/dbInit.js
+++
@@ -1,8 +0,0 @@
-
-let waterfall = openDatabase('waterfall', '1.0', 'steam', 10*1024*1024);
-
-waterfall.transaction(function(tx){
-    tx.executeSql('CREATE TABLE IF NOT EXISTS TILE (ID INTEGER PRIMARY KEY ASC, TILE_TITLE TEXT, TILE_TYPE TEXT, TILE_DESC TEXT, TILE_COVER TEXT, TILE_START INTEGER)');
-});
-
-export default waterfall;
